ff5d2a30053b20aad2eea124f772b3a1d936bba9
Sethy Rung <rungsethyhk@gmail.com>
2025-10-10 19:25:14 +0700

    fix: remove unnecessary config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,13 +21,4 @@ export default defineNuxtConfig({
   colorMode: {
     preference: "light",
   },
-  googleFonts: {
-    download: true,
-    families: {
-      Inter: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-      "Red Hat Display": [300, 400, 500, 600, 700, 800, 900],
-      "Kantumruy Pro": [100, 200, 300, 400, 500, 600, 700],
-      Battambang: [100, 300, 400, 700, 900],
-    },
-  },
 });
